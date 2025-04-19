754c057c467bcbccf2488a99919a1a405fd8a3be
wady <andreslopez261316@gmail.com>
2025-04-18 23:18:58 -0400

    Corregida URL para que funcione en Render

--- frontend/script.js
+++ frontend/script.js
@@ -52,7 +52,7 @@ async function login() {
   const username = document.getElementById('username').value;
   const password = document.getElementById('password').value;
 
-  const res = await fetch('http://localhost:3000/api/admin/login', {
+  const res = await fetch('https://formularioteamsamuraidrifters.onrender.com/api/admin/login', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ username, password }),
